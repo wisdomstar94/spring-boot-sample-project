1b3155f8b99c6d79fc8c0d0527f6e7756c48fb80
Wis Dom <wisdomstar94@gmail.com>
2022-01-31 15:10:09 +0900

    querydsl 셋팅

---
+++ src/main/generated/com/example/springbootsampleproject/entities/QUser.java
@@ -0,0 +1,43 @@
+package com.example.springbootsampleproject.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -482473111L;
+
+    public static final QUser user = new QUser("user");
+
+    public final DateTimePath<java.sql.Timestamp> createdAt = createDateTime("createdAt", java.sql.Timestamp.class);
+
+    public final NumberPath<Integer> seq = createNumber("seq", Integer.class);
+
+    public final StringPath userKey = createString("userKey");
+
+    public final StringPath userName = createString("userName");
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
